ef4004bb3aa9ad66c52916a4ff60829d0b6dcb1a
thatboycurtis <90875819+thatboycurtis@users.noreply.github.com>
2023-04-24 21:50:48 -0700

    Add files via upload

---
+++ script-file.js
@@ -0,0 +1,40 @@
+function increaseSizeDCNetflix() {
+    document.getElementById("ImgResizingDCNetflix").style.width = "25%"; // size increase
+    document.getElementById("ImgResizingDCNetflix").style.height = "40%"
+  }
+//look into resizeing at different speed rates. like resize at .5 speed
+function resetSizeDCNetflix() {
+    document.getElementById("ImgResizingDCNetflix").style.height = "35%"; // reset to original size
+    document.getElementById("ImgResizingDCNetflix").style.width = "15%"; 
+  }
+
+function increaseSizeHandwash() {
+    document.getElementById("ImgResizingHandwash").style.width = "25%"; // size increase
+    document.getElementById("ImgResizingHandwash").style.height = "40%";
+  }
+   
+function resetSizeHandwash() {
+    document.getElementById("ImgResizingHandwash").style.height = "35%"; // reset to original size
+    document.getElementById("ImgResizingHandwash").style.width = "15%"; 
+  }
+
+function increaseSizeDataSQL() {
+    document.getElementById("ImgResizingDataSQL").style.width = "25%"; // size increase
+    document.getElementById("ImgResizingDataSQL").style.height = "40%";
+  }
+   
+function resetSizeDataSQL() {
+    document.getElementById("ImgResizingDataSQL").style.height = "35%"; // reset to original size
+    document.getElementById("ImgResizingDataSQL").style.width = "15%"; 
+  }
+
+function increaseSizeIntSQL() {
+    document.getElementById("ImgResizingIntSQL").style.width = "25%"; // size increase
+    document.getElementById("ImgResizingIntSQL").style.height = "40%";
+  }
+   
+function resetSizeIntSQL() {
+    document.getElementById("ImgResizingIntSQL").style.height = "35%"; // reset to original size
+    document.getElementById("ImgResizingIntSQL").style.width = "15%"; 
+  }
+  
